8cd7948b408dbc05c1ab9c43cbc5d1b8c48e6521
DanieleAmodio <amodio_daniele8990@live.it>
2025-04-07 16:46:46 +0200

    prove

--- prova_log_in.js
+++ prova_log_in.js
@@ -6,7 +6,27 @@ if (controlloUtente) {
     let nomeUtente= "lobotka"
     let password= "segreto"
     let parolaChiave="forzanapoli"
-do  { let indovina = prompt (" Inserisci la pass segreta")
+    let contatore = ciclo(tentativo,password)
+    console.log(contatore)
+    tentativo+=contatore
+  if (tentativo === 3 ) {
+      console.log(`Hai finito i tentativi, esegui la procedura per recuperare la password` )
+      let recupero= prompt(" Hai finito i tentativi, esegui la procedura per recuperare la password. Inserisci la tua parola chiave")
+      if (recupero===parolaChiave){
+       password=prompt("inserisci la nuova password")
+        console.log(`la tua nuova password è ${password}`)
+      } else{ console.log("ci dispiace non può effettuare il recupero,se ha dimenticato la sua parola chiave effettui di nuovo la registrazione " )}
+
+  } else {
+     nomeUtente = prompt("inserisci il Nome Utente ")
+     passkey = prompt("inserisci la tua PassKey ")
+     parolaChiave = prompt("inserisci la tua Parola chiave ")
+  }
+
+}
+function ciclo ( tentativo,password)  {
+    let contatore=0
+  do  { let indovina = prompt (" Inserisci la pass segreta")
     if (indovina === password) {
         console.log("Puoi entrare!")
         break
@@ -18,19 +38,6 @@ do  { let indovina = prompt (" Inserisci la pass segreta")
       tentativo++
       console.log( `Hai a disposizione altri ${3-tentativo}`)
 } while (tentativo <3)
-  if (tentativo === 3 ) {
-      console.log(`Hai finito i tentativi, esegui la procedura per recuperare la password` )
-      let recupero= prompt(" Hai finito i tentativi, esegui la procedura per recuperare la password. Inserisci la tua parola chiave")
-      if (recupero===parolaChiave){
-        password=prompt("inserisci la nuova password")
-        console.log(`la tua nuova password è ${password}`)
-      } else{ console.log("ci dispiace non può effettuare il recupero,se ha dimenticato la sua parola chiave effettui di nuovo la registrazione " )}
-
-  } else {
-     nomeUtente = prompt("inserisci il Nome Utente ")
-     passkey = prompt("inserisci la tua PassKey ")
-     parolaChiave = prompt("inserisci la tua Parola chiave ")
-  }
-
-}
-
+   contatore+=tentativo
+   return contatore
+}; 
