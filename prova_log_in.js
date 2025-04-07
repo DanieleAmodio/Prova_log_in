2bada90cdea369a86e30d9c0ed8e77a33b8c2282
DanieleAmodio <amodio_daniele8990@live.it>
2025-04-07 17:48:43 +0200

    funziona

--- prova_log_in.js
+++ prova_log_in.js
@@ -1,32 +1,45 @@
-
-let controlloUtente= prompt ("sei registrato? (si o no)")
-if (controlloUtente) {
+let tentativo = 0
+let nomeUtente= "lobotka"
+let password= "segreto"
+let parolaChiave="forzanapoli"
+let controlloUtente= prompt ("sei registrato? (inserisci Nome Utente altrimenti No")
+if (controlloUtente===nomeUtente) {
     console.log(controlloUtente)
-    let tentativo = 0
-    let nomeUtente= "lobotka"
-    let password= "segreto"
-    let parolaChiave="forzanapoli"
-    let contatore = ciclo(tentativo,password)
+    let contatore = ciclo(tentativo,password,nomeUtente)
     console.log(contatore)
     tentativo+=contatore
-  if (tentativo === 3 ) {
-      console.log(`Hai finito i tentativi, esegui la procedura per recuperare la password` )
-      let recupero= prompt(" Hai finito i tentativi, esegui la procedura per recuperare la password. Inserisci la tua parola chiave")
-      if (recupero===parolaChiave){
-       password=prompt("inserisci la nuova password")
-        console.log(`la tua nuova password è ${password}`)
-      } else{ console.log("ci dispiace non può effettuare il recupero,se ha dimenticato la sua parola chiave effettui di nuovo la registrazione " )}
-
+    let controllo = controllato(nomeUtente,password,parolaChiave,tentativo)
   } else {
-     nomeUtente = prompt("inserisci il Nome Utente ")
-     passkey = prompt("inserisci la tua PassKey ")
-     parolaChiave = prompt("inserisci la tua Parola chiave ")
-  }
+  nomeUtente = prompt("inserisci il Nome Utente ")
+  password= prompt("inserisci la tua Password ")
+  parolaChiave = prompt("inserisci la tua Parola chiave ")
+  console.log(`Ciao ${nomeUtente}`)
+  contatore=ciclo(tentativo,password,nomeUtente)
+  tentativo+=contatore
+  controllo = controllato(nomeUtente,password,parolaChiave,tentativo)
 
 }
+
+function controllato(nomeUtente,password,parolaChiave,tentativo) {
+  if (tentativo === 3 ) {
+    console.log(`Hai finito i tentativi, esegui la procedura per recuperare la password` )
+    let recupero= prompt(" Hai finito i tentativi, esegui la procedura per recuperare la password. Inserisci la tua parola chiave")
+    if (recupero===parolaChiave){
+     password=prompt("inserisci la nuova password")
+      console.log(`la tua nuova password è ${password}`)
+      tentativo=0
+      contatore=ciclo(tentativo,password)
+      tentativo+=contatore
+      if (tentativo === 3 ) { console.log("Hai finito i tentativi e la password e stata cambiata di recente.Contattare l'assistenza ")}
+
+    } else{ console.log("ci dispiace non può effettuare il recupero,se ha dimenticato la sua parola chiave effettui di nuovo la registrazione " )}
+
+} 
+}
+
 function ciclo ( tentativo,password)  {
     let contatore=0
-  do  { let indovina = prompt (" Inserisci la pass segreta")
+  do  { let indovina = prompt (` Ciao ${nomeUtente} Inserisci la password `)
     if (indovina === password) {
         console.log("Puoi entrare!")
         break
@@ -41,3 +54,4 @@ function ciclo ( tentativo,password)  {
    contatore+=tentativo
    return contatore
 }; 
+
